a9d43fe1b2d197bb7521ddd095702aab99a59811
Ixora <marionlabbe@posteo.net>
2025-01-06 16:37:50 +0100

    [FIX] accès au répertoire /admin [ADD] netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://deft-churros-b41b32.netlify.app',
   integrations: [mdx(), sitemap()],
   adapter: netlify(),
 });
